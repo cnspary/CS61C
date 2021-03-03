1ff9161c2a9c060af17a7c60c017720cb6a2d6b8
wangxingyu <wangxingyu@zju.edu.cn>
2021-03-03 17:17:14 +0800

    lab2 done

--- su20-lab-starter/lab02/lfsr.c
+++ su20-lab-starter/lab02/lfsr.c
@@ -4,7 +4,14 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
+void lfsr_calculate(uint16_t* reg) {
     /* YOUR CODE HERE */
+    unsigned short bit0, bit2, bit3, bit5, MSB;
+    bit0 = (*reg) & 1;
+    bit2 = (*reg >> 2) & 1;
+    bit3 = (*reg >> 3) & 1;
+    bit5 = (*reg >> 5) & 1;
+    MSB = ((bit0 ^ bit2 ^ bit3 ^ bit5) << 15);
+    *reg = (*reg >> 1) | MSB;
 }
 
---
+++ su20-lab-starter/lab02/make.md
@@ -0,0 +1,7 @@
+1. **make clean**
+2. **all: $(BINARIES)**
+3. **CC=gcc**
+4. **-std=c99**
+5. **$(FOO)**
+6. **Unix**
+7. **line 31**
